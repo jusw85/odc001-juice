493fdd75d2eeea54e95b49858f43e4f904662b54
ziczach <czc85@hotmail.com>
2017-10-18 16:25:33 +0800

    Added Audio Manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //for implmentation, i.e after gameobject is destroyed, add in line below
+    //FindObjectofType<AudioManager>().Play("Explosion");
+
+    public Sound[] sounds; 
+
+	void Awake()
+    { 
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            //s.source.loop = s.loop;
+        }
+	}
+
+    void Start()
+    {
+        //Play("StartMusic");
+    }
+
+	// Update is called once per frame
+	//public void Play (string name)
+    //{
+        //Sound s = Array.Find(sounds, sound => sound.name == name);
+       // if (s == null) ;
+        //return;
+        //s.source.Play();
+		
+	//}
+}
