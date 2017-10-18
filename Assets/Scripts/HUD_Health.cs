c5a69218ea7d946e632429a8ba8b40c756b5d5a5
ziczach <czc85@hotmail.com>
2017-10-18 13:11:40 +0800

    Update Main HUD
    added heart sprites.added player life.added score manager. added hud_health

---
+++ Assets/Scripts/HUD_Health.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUD_Health : MonoBehaviour
+{
+
+    public Sprite[] HeartSprites;
+
+    public Image HeartUI;
+    
+    //To be referenced to PlayerController script
+    //public PlayerController player;
+
+    // Use this for initialization
+    void Start ()
+    {
+       // player = FindObjectOfType<PlayerController>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //curHealth to be referenced to PlayerController script. int value. I.e curHealth 5 - sprite no.5, curHealth 4 - sprite no.4.
+        //HeartUI.sprite = HeartSprites[player.curHealth];
+	}
+}
